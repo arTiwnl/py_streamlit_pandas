eb0ddccba34897846a66d423f9e6c46539fc493b
artiwnl <contatocarvalhoarthur@gmail.com>
2025-03-04 13:52:03 -0300

    feat: importing streamlit and pandas

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_topbooks = pd.read_csv('datasets/Top-100 Trending Books.csv')
+
+df_topbooks
