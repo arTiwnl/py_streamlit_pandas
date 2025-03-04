f78c7589b34a3bb877cf040297e48878d4ccfcfb
artiwnl <contatocarvalhoarthur@gmail.com>
2025-03-04 15:18:37 -0300

    feat:code finished

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_topbooks = pd.read_csv('datasets/Top-100 Trending Books.csv')
 
-df_topbooks
+price_max = df_topbooks["book price"].max()
+price_min = df_topbooks["book price"].min()
+
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
+df_books = df_topbooks[df_topbooks["book price"] <= max_price]
+df_books
+
+fig = px.bar(df_books["year of publication"].value_counts())
+fig2 = px.histogram(df_books["book price"])
+
+col1, col2 = st.columns(2)
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
+
